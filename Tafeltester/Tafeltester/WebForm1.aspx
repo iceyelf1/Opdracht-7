664ef357ee15f84c124be59c347ca3c17c4708a2
iceyelf1 <josha-wesselink@hotmail.com>
2017-04-20 17:53:09 +0200

    Opdracht 7

---
+++ Tafeltester/Tafeltester/WebForm1.aspx
@@ -0,0 +1,69 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Tafeltester.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>Tafeltester</h1>
+        <p>Vul het hoogste getal in dat gebruikt mag worden in de sommen:
+            <asp:TextBox ID="tbHoogsteGetal" runat="server"></asp:TextBox>
+&nbsp;</p>
+        <p>
+            <asp:Button ID="btnMaakSom" runat="server" OnClick="btnMaakSom_Click" Text="Maak Sommen" />
+        </p>
+        <p>Sommen:</p>
+        <p>
+            1 x
+            <asp:Label ID="lblSom1" runat="server"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; =&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="tbSom1" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp; 
+            <asp:Label ID="lblUitkomst1" runat="server"></asp:Label>
+        </p>
+        <p>
+            2 x
+            <asp:Label ID="lblSom2" runat="server"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; =&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="tbSom2" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp; 
+            <asp:Label ID="lblUitkomst2" runat="server"></asp:Label>
+        </p>
+        <p>
+            3 x
+            <asp:Label ID="lblSom3" runat="server"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; =&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="tbSom3" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp; 
+            <asp:Label ID="lblUitkomst3" runat="server"></asp:Label>
+        </p>
+        <p>
+            4 x
+            <asp:Label ID="lblSom4" runat="server"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; =&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="tbSom4" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp; 
+            <asp:Label ID="lblUitkomst4" runat="server"></asp:Label>
+        </p>
+        <p>
+            5 x
+            <asp:Label ID="lblSom5" runat="server"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; =&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="tbSom5" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp; 
+            <asp:Label ID="lblUitkomst5" runat="server"></asp:Label>
+        </p>
+        <p>
+            <asp:Button ID="btnScore" runat="server" Text="Laat score zien" OnClick="btnScore_Click" />
+        </p>
+        <p>
+            <asp:Label ID="lblScore" runat="server" Font-Names="Forte" Font-Size="30pt"></asp:Label>
+        </p>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Tafeltester/Tafeltester/WebForm1.aspx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Tafeltester
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnMaakSom_Click(object sender, EventArgs e)
+        {
+            Random som = new Random();
+            int hg = Convert.ToInt32(tbHoogsteGetal.Text);
+            lblSom1.Text = Convert.ToString(som.Next(hg)+1);
+            lblSom2.Text = Convert.ToString(som.Next(hg)+1);
+            lblSom3.Text = Convert.ToString(som.Next(hg)+1);
+            lblSom4.Text = Convert.ToString(som.Next(hg)+1);
+            lblSom5.Text = Convert.ToString(som.Next(hg)+1);
+        }
+
+        protected void btnScore_Click(object sender, EventArgs e)
+        {
+            int eindScore = 10;
+
+            if (tbSom1.Text == Convert.ToString(Convert.ToInt32(lblSom1.Text) * 1))
+            {
+                lblUitkomst1.Text = "Goed";
+            }
+
+            else
+            {
+                lblUitkomst1.Text = "Fout";
+                eindScore = eindScore - 2;
+            }
+///////////////////////////////////////////////////////////////////////////////////////////
+            if (tbSom2.Text == Convert.ToString(Convert.ToInt32(lblSom2.Text) * 2))
+            {
+                lblUitkomst2.Text = "Goed";
+            }
+
+            else
+            {
+                lblUitkomst2.Text = "Fout";
+                eindScore = eindScore - 2;
+            }
+///////////////////////////////////////////////////////////////////////////////////////////
+            if (tbSom3.Text == Convert.ToString(Convert.ToInt32(lblSom3.Text) * 3))
+            {
+                lblUitkomst3.Text = "Goed";
+            }
+
+            else
+            {
+                lblUitkomst3.Text = "Fout";
+                eindScore = eindScore - 2;
+            }
+///////////////////////////////////////////////////////////////////////////////////////////
+            if (tbSom4.Text == Convert.ToString(Convert.ToInt32(lblSom4.Text) * 4))
+            {
+                lblUitkomst4.Text = "Goed";
+            }
+
+            else
+            {
+                lblUitkomst4.Text = "Fout";
+                eindScore = eindScore - 2;
+            }
+///////////////////////////////////////////////////////////////////////////////////////////
+            if (tbSom5.Text == Convert.ToString(Convert.ToInt32(lblSom5.Text) * 5))
+            {
+                lblUitkomst5.Text = "Goed";
+            }
+
+            else
+            {
+                lblUitkomst5.Text = "Fout";
+                eindScore = eindScore - 2;
+            }
+            ///////////////////////////////////////////////////////////////////////////////////////////
+            lblScore.Text = "Je hebt een" + "&nbsp" + Convert.ToString(eindScore);
+        }
+    }
+}
